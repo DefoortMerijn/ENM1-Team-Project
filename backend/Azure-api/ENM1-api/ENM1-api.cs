21ce1ec77e1a34a9671eba77f77fa2cd403733bf
Aaron <aaron.carton@student.howest.be>
2022-01-19 10:50:50 +0100

    Update ENM1-api.cs

--- backend/Azure-api/ENM1-api/ENM1-api.cs
+++ backend/Azure-api/ENM1-api/ENM1-api.cs
@@ -1,5 +1,4 @@
 using System;
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
@@ -21,49 +20,81 @@ namespace ENM1_api
         static string BUCKET = Environment.GetEnvironmentVariable("InfluxDB_bucket");
         static string ORG = Environment.GetEnvironmentVariable("InfluxDB_org");
 
+        // Dict of time types along with their respective duration units
+        static Dictionary<string, string[]> pairs = new Dictionary<string, string[]>()
+            {
+                { "year", new string[] { "-1y", "1mo" } }, // shows data up until 1 year ago, grouped per month
+                { "month", new string[] { "-1mo", "1d" } },
+                { "week", new string[] { "-1w", "1d" } },
+                { "day", new string[] { "-1d", "1h" } },
+                { "recent", new string[] { "-1h", "5m" } },
+            };
+
+        // List of fields to exclude from query
+        static string[] blacklist = { "CO2_5min", "Description_Weather", "Humidity", "Pressure", "Temperature", "Windspeed" };
+        static string strBlacklist = $"[{string.Join(", ", blacklist.Select(x => string.Format("\"{0}\"", x)))}]"; // string[] obj => ["x", "z", "y"] because C# is dumb
+
+
         [FunctionName("GetFields")]
         public static async Task<IActionResult> GetFields(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "power/fields")] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "power/duiktank/fields")] HttpRequest req,
             ILogger log)
         {
             using var client = InfluxDBClientFactory.Create(URL, TOKEN);
-
             var query = "import \"influxdata/influxdb/schema\" "
                       + "import \"json\""
                       + "schema.fieldKeys(bucket: \"Transfosite\")";
-
             var tables = await client.GetQueryApi().QueryAsync(query, ORG);
+
             List<string> fields = tables.SelectMany(table => table.Records).Select(record => (string)record.GetValue()).ToList();
-            var json = JsonConvert.SerializeObject(fields);
+            fields.RemoveAll(f => blacklist.Contains(f)); // filter blacklisted fields
 
             return new JsonResult(fields);
         }
 
-        [FunctionName("GetRecentPowerUsage")]
-        public static async Task<IActionResult> GetRecentPowerUsage(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "power/usage/recent/{field}")] HttpRequest req, ILogger log, string field)
+        [FunctionName("GetPowerUsageDuiktank")]
+        public static async Task<IActionResult> GetPowerUsageDuiktank(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "power/duiktank/usage/{time}/{field?}")] HttpRequest req, ILogger log, string time, string field)
         {
+            // Check whether correct time parameter was given, otherwise return BadRequest
+            if (!pairs.ContainsKey(time)) return new BadRequestObjectResult(new { http_code = 400, error_message = "Incorrect time parameter, please check documentation" });
+
+            // get range and window from given time parameter
+            string range = pairs[time][0];
+            string window = pairs[time][1];
+
+            // create query
             using var client = InfluxDBClientFactory.Create(URL, TOKEN);
             var query = $"from(bucket: \"{BUCKET}\")"
-                       + "   |> range(start: -1h)"
-                       +$"   |> filter(fn: (r) => r._field == \"{field}\")";
-
+                       + $"\n|> range(start: {range})" // if field param was given -> filter on field, else just filter on blacklist
+                       + $"\n|> filter(fn: (r) => {(field != null ? $"r._field == \"{field}\"" : $"not contains(value: r._field, set: {strBlacklist})")})"
+                       + $"\n|> aggregateWindow(every: {window}, fn: sum)";
             var tables = await client.GetQueryApi().QueryAsync(query, ORG);
 
-            if (tables.Count == 0) return new BadRequestObjectResult(new { http_code = 400, error_message = "Table not found, check if given name is correct (case sensitive)" });
+            // check for found tables, otherwise return BadRequest
+            if (tables.Count == 0) return new BadRequestObjectResult(new { http_code = 400, error_message = "No data found, check if given field parameter is correct (case sensitive)" });
 
+
+            // format to JSON
             var records = tables.SelectMany(table => table.Records).ToList();
             var json = new
             {
                 http_code = 200,
                 info = new
                 {
-                    field = records[0].GetField(),
                     measurement = records[0].GetMeasurement(),
                     meter = records[0].GetValueByKey("meter")
                 },
-                values = records.Select(r => new { time = r.GetTime(), value = r.GetValue() })
+                data = records
+                         .OrderBy(r => r.GetField())
+                         .GroupBy(r => r.GetField())
+                         .Select(x => new
+                         {
+                             Field = x.Key,
+                             values = x.Select(y => new { time = y.GetTimeInDateTime(), value = y.GetValue() })
+                         })
             };
+
             return new JsonResult(json);
         }
     }
