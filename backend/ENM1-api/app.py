0d92ffb165aa592927c4d234581cfc4ca284ba34
Aaron <aaron.carton@student.howest.be>
2022-01-23 23:35:48 +0100

    Create fields route, duiktank power_usage route

--- backend/ENM1-api/app.py
+++ backend/ENM1-api/app.py
@@ -1,10 +1,14 @@
+from collections import defaultdict
+from email.policy import default
+from itertools import groupby
 from flask import Flask, jsonify, request, url_for, Response, session
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 from datetime import datetime
 from flask_cors import CORS
 from flask_mqtt import Mqtt
-import os, sys, yaml
+from functools import reduce
+import os, sys, yaml, json
 
 # load config file
 if not os.path.isfile("config.yaml"):
@@ -14,16 +18,26 @@ else:
     config = yaml.load(file, Loader=yaml.FullLoader)
 
 
-# Start app
+# start app
 app = Flask(__name__)
+
+# enviroment variables
 app.config['SECRET_KEY'] = config['Flask']['SECRET_KEY']
 app.config['MQTT_BROKER_URL'] = config['MQTT']['BROKER_URL']
 app.config['MQTT_BROKER_PORT'] = config['MQTT']['BROKER_PORT']
 app.config['MQTT_REFRESH_TIME'] = 1.0  # refresh time in seconds
-Url = config['InfluxDB']['Howest']['URL']
-token = config['InfluxDB']['Howest']['TOKEN']
-org = config['InfluxDB']['Howest']['ORG']
-bucket = config['InfluxDB']['Howest']['BUCKET']
+
+Influx_howest = config['InfluxDB']['Howest']
+Influx_transfo = config['InfluxDB']['Transfo']
+field_blacklist = [ "CO2_5min", "Description_Weather", "Humidity", "Pressure", "Temperature", "Windspeed" ]
+time_ranges = {
+    "year": ["-1y", "1mo"],
+    "month": ["-1mo", "1d"],
+    "week": ["-1w", "1d"],
+    "day": ["-1d", "1h"],
+    "recent": ["-1h", "5m"],
+}
+
 mqtt = Mqtt(app)
 CORS(app)
 
@@ -32,20 +46,52 @@ CORS(app)
 endpoint = '/api/v1'
 
 
-# ROUTES
+###### FLASK ROUTES ######
 @app.route('/')
 def index():
     return 'use /api/v1', 303
 
+# get all possible fields from duiktank influxdb
+@app.route(f'{endpoint}/power/duiktank/fields', methods=['GET'])
+def get_duiktank_fields():
+    # get Transfo values
+    URL, TOKEN, ORG, BUCKET = Influx_transfo.values()
 
-# flask routes
-@app.route(f'{endpoint}/transfo', methods=['GET'])
-def anime_favorites():
-    # get params
-    key = request.values.get('key')
+    with InfluxDBClient(url=URL, token=TOKEN, org=ORG) as client:
+        query = f'import "influxdata/influxdb/schema" schema.fieldKeys(bucket: "{BUCKET}")'
+        tables = client.query_api().query(query, org=ORG)
+        client.close()
 
-    if request.method == 'GET':
-        return jsonify(status_code=200, message="success"), 200
+        fields = [r.values['_value'] for r in tables[0].records] # get field values
+        fields = list(filter(lambda x: x not in field_blacklist, fields)) # remove blacklisted fields
+        return jsonify(fields), 200
+
+@app.route(f'{endpoint}/power/duiktank/usage/<time>', methods=['GET'])
+@app.route(f'{endpoint}/power/duiktank/usage/<time>/<field>', methods=['GET'])
+def get_powerusage_duiktank(time, field=None):
+    # get Transfo values
+    URL, TOKEN, ORG, BUCKET = Influx_transfo.values()
+    # get range and window from given time parameter
+    range, window = time_ranges[time]
+
+    with InfluxDBClient(url=URL, token=TOKEN, org=ORG) as client:
+        
+        # if field param was given -> filter on field, else just filter on blacklist
+        query = f'''from(bucket: "{BUCKET}")
+                        |> range(start: {range})
+                        |> filter(fn: (r) => {f'r._field == "{field}"' if field is not None else f'not contains(value: r._field, set: {json.dumps(field_blacklist)})'})
+                        |> aggregateWindow(every: {window}, fn: sum)'''
+        tables = client.query_api().query(query, org=ORG)
+        client.close()
+
+        # group by field, each field contains list of respective records with time and value 
+        dict = defaultdict()
+        for table in tables:
+            for r in table.records:
+                dict.setdefault(r.values['_field'], []).append({'time': r.values['_time'], 'value': r.values['_value']})
+
+
+        return jsonify(http_code=200, info={'measurement': "Duiktank"}, values=dict), 200
 
 
 # MQTT
@@ -53,15 +99,6 @@ def anime_favorites():
 def handle_connect(client, userdata, flags, rc):
     print("Connected to Transfo MQTT broker with result code " + str(rc))
     mqtt.subscribe('/placeholder/topic')
-    print(config["test"])
-    with InfluxDBClient(url=Url, token=token, org=org) as client:
-        write_api = client.write_api(write_options=SYNCHRONOUS)
-        point = Point("mem") \
-                .tag("host", "host1") \
-                .field("used_percent", 23.43234543) \
-                .time(datetime.utcnow(), WritePrecision.NS)
-        print("Writing point: " + str(point))
-        write_api.write(bucket, org, point)
 
 @mqtt.on_message()
 def handle_mqtt_message(client, userdata, message):
