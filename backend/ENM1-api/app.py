2c44be674c727f804aea0287c564f88d4443cf29
Aaron <78962840+AaronCarton@users.noreply.github.com>
2022-01-25 11:07:40 +0100

    Fix Azure deployment
    Update feature-python-rewrite_enm1-flask.yml
    
    Possible fix for Azure deployment
    
    Update feature-python-rewrite_enm1-flask.yml

--- backend/ENM1-api/app.py
+++ backend/ENM1-api/app.py
@@ -167,6 +167,6 @@ def add_point_data(points):
 
 
 
-# start app
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+# # start app
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', debug=True)
